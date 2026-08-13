4be7b48c582cdb617b2545b68db5d80870b6d921
pklinga <pklinga0417@gmail.com>
2026-08-13 11:46:34 +0200

    BoyerMoore class started, bad char table pattern done

--- Main2.java
+++ Main2.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Implementing the Boyer-Moore string algorithm
+ */
+class BoyerMoore {
+    // ASCII characters
+    private static final int ALPHABET_SIZE = 256;
+
+    /**
+     * Pattern to create bad character table
+     * 
+     * @param pattern
+     * @return Array
+     */
+    private static int[] badCharHeuristic(String pattern) {
+        int m = pattern.length();
+        int[] badChar = new int[ALPHABET_SIZE];
+
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            badChar[i] = -1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            badChar[(int) pattern.charAt(i)] = i;
+        }
+        return badChar;
+    }
+
+}
 
 public class Main2 {
 
