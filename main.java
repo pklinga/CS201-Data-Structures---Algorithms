9894c0c23c849445b2f10266bb8dbaf47dc0bba2
pklinga <pklinga0417@gmail.com>
2026-08-13 08:12:48 +0200

    first commit

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * The binary Search Tree node class
+ */
+
+class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
